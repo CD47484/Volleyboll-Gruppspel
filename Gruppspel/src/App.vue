1d4f5fcc723ec5df8609cb56ae467bc82d97d65d
CD47484 <47484@gymnasium.ax>
2024-03-26 12:30:10 +0200

    test

--- Gruppspel/src/App.vue
+++ Gruppspel/src/App.vue
@@ -3,9 +3,41 @@
 </script>
 
 <template>
+<nav id="nav">Massor med saker</nav>
 
+<main id="main">
+<h1>Gruppspel</h1>
+<br>
+
+<h3>Grupp 1</h3> <!--fetchar in grupperna-->
+
+<br>
+
+<h3>Grupp 2</h3> 
+
+<br>
+
+<h3>Grupp 3</h3>
+</main>
 </template>
 
 <style scoped>
-
+#nav {
+  position: absolute;
+  display: flex;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  height: 10%;
+  align-items: center;
+  background-color: darkgreen;
+}
+#main {
+  position: absolute;
+  top: 10%;
+  bottom: 0%;
+  left: 0%;
+  right: 0%;
+  background-color: darkblue;
+}
 </style>
